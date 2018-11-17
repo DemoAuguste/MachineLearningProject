7af29001e4955fb85bdddb195249362dc10cb300
DemoAuguste <393493772@qq.com>
2018-11-17 21:05:39 +0800

    some modification

--- models/Binary_classifier.py
+++ models/Binary_classifier.py
@@ -7,13 +7,15 @@ from sklearn import metrics
 import copy
 
 x_train,y_train,x_test,y_test = load_data(normalize=True)
-classifiers = ['knn_classifier','logistic_regression_classifier','decision_tree_classifier','svm_classifier','cnn_classifier','dnn_classifier']
+
+# 这个地方去掉了CNN和RNN，因为在前面的模型中这两个模型最后输出的格式为[0,0,0,0,0,0,0,0,0,0]。
+# 如果要考虑这两个模型，需要修改这两个模型最后的输出层
+classifiers = ['knn_classifier','logistic_regression_classifier','decision_tree_classifier','svm_classifier']
 
 def binary_training(model,one_hot=False,num_classes=2):
 	binary_trained_model = {}
 	for i in range(10):
 		for j in range(i+1,10):
-#			print('training {},{}'.format(i,j))
 			temp_train_x = np.row_stack((x_train[200*i:200*(i+1)],x_train[200*j:200*(j+1)]))
 			if one_hot:
 				label_1 = np.array([[1,0] for k in range(200)])
@@ -25,9 +27,10 @@ def binary_training(model,one_hot=False,num_classes=2):
 			
 			temp_model = eval(model)(temp_train_x,temp_train_y,num_classes)
 			
-			y_eval = temp_model.predict(temp_train_x)
-			training_accuracy = metrics.accuracy_score(temp_train_y,y_eval)
-			print('training accuracy: {}'.format("%.2f%%"%( 100*training_accuracy)))
+			# evaluating training accuracy
+#			y_eval = temp_model.predict(temp_train_x)
+#			training_accuracy = metrics.accuracy_score(temp_train_y,y_eval)
+#			print('training accuracy: {}'.format("%.2f%%"%( 100*training_accuracy)))
 			
 			binary_trained_model[str(i)+'_'+str(j)] = copy.deepcopy(temp_model)
 	return binary_trained_model
@@ -57,12 +60,21 @@ def binary_evaluation(model,test_data_x,test_data_y):
 			count += 1
 
 	print("Accuracy: {}".format(count/len(test_data_x)))
+	
+	
+def all_model_evaluation():
+	for classifier in classifiers:
+		print('=========={}=========='.format(classifier))
+		ret = binary_training(classifier,one_hot=False)
+		binary_evaluation(ret, x_test, y_test)
 			
 	
 if __name__ == '__main__':
-	classifiers = ['knn_classifier','logistic_regression_classifier','decision_tree_classifier','svm_classifier','cnn_classifier','dnn_classifier']
-	ret = binary_training('svm_classifier',one_hot=False)
-	binary_evaluation(ret, x_test, y_test)
+	all_model_evaluation()
+	
+	
+#	ret = binary_training('knn_classifier',one_hot=False)
+#	binary_evaluation(ret, x_test, y_test)
 	
 #	m = ret[str(1) + '_' + str(5)]
 #	ret = m.predict([x_test[201]])
